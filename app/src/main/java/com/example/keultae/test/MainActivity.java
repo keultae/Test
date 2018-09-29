49ae187e3878ec872f70a8706db56d01f85b876c
keultae <keultae@korea.com>
2018-09-29 17:21:06 +0900

    git test

--- app/src/main/java/com/example/keultae/test/MainActivity.java
+++ app/src/main/java/com/example/keultae/test/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.keultae.test;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * Git 테스트를 위해 주석 추가
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
